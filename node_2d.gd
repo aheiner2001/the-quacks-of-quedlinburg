b477ea05d54c7eee2ddd2173e1ac0e894f45adf5
Aaron Heiner <aheiner2001@gmail.com>
2026-07-25 14:57:58 -0700

    Initial commit

---
+++ node_2d.gd
@@ -0,0 +1,22 @@
+extends Node2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	# Automatically apply the mask to all your bottle buttons here
+	setup_bottle_mask($TextureButton)
+	setup_bottle_mask($TextureButton2)
+
+# Reusable helper function to keep your code clean
+func setup_bottle_mask(btn: TextureButton) -> void:
+	if btn and btn.texture_normal:
+		var img = btn.texture_normal.get_image()
+		var bm = BitMap.new()
+		bm.create_from_image_alpha(img)
+		btn.texture_click_mask = bm
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	pass
+
+func _on_reveal_button_pressed() -> void:
+	$RevealImage.visible = !$RevealImage.visible
