09b6e0d4642139d2a831c8322443ad217802310a
Aaron Heiner <aheiner2001@gmail.com>
2026-07-27 10:36:28 -0600

    fix: rename Chip.Color to ChipColor to avoid Godot Color clash

--- game/chip.gd
+++ game/chip.gd
@@ -1,10 +1,10 @@
 class_name Chip
 extends RefCounted
 
-enum Color { WHITE, ORANGE, GREEN, BLUE, RED, YELLOW, PURPLE, BLACK }
+enum ChipColor { WHITE, ORANGE, GREEN, BLUE, RED, YELLOW, PURPLE, BLACK }
 
 static func make(color: int, value: int) -> Dictionary:
 	return {"color": color, "value": value}
 
 static func is_white(chip: Dictionary) -> bool:
-	return int(chip["color"]) == Color.WHITE
+	return int(chip["color"]) == ChipColor.WHITE
--- tests/test_chip.gd
+++ tests/test_chip.gd
@@ -3,9 +3,9 @@ extends RefCounted
 
 static func run() -> int:
 	var f := 0
-	var c := Chip.make(Chip.Color.WHITE, 2)
-	f += AssertUtil.eq(c["color"], Chip.Color.WHITE, "chip color")
+	var c := Chip.make(Chip.ChipColor.WHITE, 2)
+	f += AssertUtil.eq(c["color"], Chip.ChipColor.WHITE, "chip color")
 	f += AssertUtil.eq(c["value"], 2, "chip value")
 	f += AssertUtil.truthy(Chip.is_white(c), "white check")
-	f += AssertUtil.eq(Chip.is_white(Chip.make(Chip.Color.ORANGE, 1)), false, "orange not white")
+	f += AssertUtil.eq(Chip.is_white(Chip.make(Chip.ChipColor.ORANGE, 1)), false, "orange not white")
 	return f
